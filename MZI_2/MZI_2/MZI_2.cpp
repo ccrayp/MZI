b74ca10cc8230ac9c7c9abc790d2a447af15011f
roman@M1402 <demixhd2@gmail.com>
2024-11-06 19:53:42 +0300

    MZI_2

---
+++ MZI_2/MZI_2/MZI_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+
+typedef unsigned int uint;
+
+std::vector<int> LCG(uint n);
+
+int main()
+{
+    uint n = 0;
+    std::cin >> n;
+}
+
+std::vector<int> LCG(uint n) {
+
+    return std::vector<int>();
+}
