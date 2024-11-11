2d67174a7375a427337eb110a01e9a28a075c9d3
roman@M1402 <demixhd2@gmail.com>
2024-11-11 20:18:34 +0300

    update MZI_2.cpp

--- MZI_2/MZI_2/MZI_2.cpp
+++ MZI_2/MZI_2/MZI_2.cpp
@@ -1,15 +1,15 @@
 ﻿#include <iostream>
-#include <vector>
-#include <string>
 #include <fstream>
+
+#include "LCG.h"
+#include "BBS.h"
+#include "LFSR.h"
 
 typedef unsigned int uint;
 
-bool is_prime(uint a, uint b);
-std::vector<int> LCG(uint n, uint seed, uint m, uint a, uint c);
-std::string output_vector(std::vector<int> v);
-std::vector<int> factorise(int m);
 int LCG_init();
+int BBS_init();
+int LFSR_init();
 
 int main()
 {
@@ -18,82 +18,57 @@ int main()
 
 int LCG_init() {
     uint n = 0, seed = 0, a = 0, c = 0, m = 0;
-    std::cout << "Enter n: ";
+    std::cout << "LCG generator\n\nEnter n: ";
     std::cin >> n;
-    std::cout << "Enter seed: ";
-    std::cin >> seed;
-    std::cout << "Enter m: ";
-    std::cin >> m;
-    std::cout << "Enter a: ";
-    std::cin >> a;
-    std::cout << "Enter c: ";
-    std::cin >> c;
-    if (m < 2) {
-        std::cout << "Too little value of m\n";
+    std::ofstream out("generated_LCG.txt");
+    if (!out.is_open()) {
+        std::cout << "File was not opened\n";
         return 1;
     }
-    if (a > m || a < 0) {
-        std::cout << "Incorrect value of a\n";
-        return 2;
+    std::cout << std::endl;
+    LCG LCG;
+    for (int i = 0; i < n; i++) {
+        std::cout << LCG.generate() << " ";
     }
-    if (c > m || c < 0) {
-        std::cout << "Incorrect value of c\n";
-        return 3;
-    }
-    if (!is_prime(c, m)) {
-        std::cout << "c and m is not prime\n";
-        return 4;
-    }
-    int b = a - 1;
-    if (m % 4 == 0 && b % 4 != 0) {
-        std::cout << "c and m is not prime\n";
-        return 5;
-    }
-    std::ofstream out("generated.txt", std::ios_base::binary);
+    std::cout << std::endl;
+
+    return 0;
+}
+
+int BBS_init() {
+    std::cout << "BBS generator\n\nEnter n: ";
+    uint n = 0;
+    std::cin >> n;
+    std::ofstream out("generated_BBS.txt");
     if (!out.is_open()) {
-        std::cout << "File ws not opened\n";
-        return 6;
+        std::cout << "File was not opened\n";
+        return 1;
     }
-
-    std::vector<int> gen = LCG(n, seed, m, a, c);
-    for (auto val : gen) {
-        out.write(reinterpret_cast<char*>(&val), sizeof(int));
+    std::cout << std::endl;
+    BBS BBS;
+    for (int i = 0; i < n; i++) {
+        std::cout << BBS.generate() << " ";
     }
-    out.close();
+    std::cout << std::endl;
 
-    std::cout << output_vector(gen) << std::endl;
+    return 0;
 }
 
-bool is_prime(uint a, uint b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
+int LFSR_init() {
+    uint n = 0, seed = 0, p = 0, q = 0;
+    std::cout << "LFSR generator\n\nEnter n: ";
+    std::cin >> n;
+    std::ofstream out("generated_BBS.txt");
+    if (!out.is_open()) {
+        std::cout << "File was not opened\n";
+        return 1;
     }
-
-    if (a == 1)
-        return true;
-    else
-        return false;
-}
-
-std::vector<int> LCG(uint n, uint seed, uint m, uint a, uint c) {
-    std::vector<int> numbers(n);
-    numbers[0] = seed;
-    for (int i = 0; i < n - 1; i++) {
-        numbers[i + 1] = (a * numbers[i] + c) % m;
+    std::cout << std::endl;
+    LFSR LFSR;
+    for (int i = 0; i < n; i++) {
+        std::cout << LFSR.generate() << " ";
     }
+    std::cout << std::endl;
 
-    return numbers;
-}
-
-std::string output_vector(std::vector<int> v) {
-    std::string res = "";
-    for (auto n : v)
-        res += std::to_string(n) + " ";
-    return res + '\n';
-}
-
-std::vector<int> factorise(int m) {
-    return std::vector<int>();
+    return 0;
 }
